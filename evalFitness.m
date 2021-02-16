aa571c84a139e6f8090e6bce33809df4df9c60b8
Elliot Stockwell <epstockwell@charter.net>
2021-02-16 17:04:16 -0500

    Create evalFitness.m

---
+++ evalFitness.m
@@ -0,0 +1,9 @@
+function fitnessVal = evalFitness(x, bearing_diameter, ...
+    planet_shaft_diameter, sun_shaft_diameter, maxX, GR_front_target, ...
+    GR_rear_target)
+
+phi = fval(x, GR_front_target, GR_rear_target);
+penalty = constraint(x, bearing_diameter, planet_shaft_diameter, ...
+    sun_shaft_diameter, maxX);
+
+fitnessVal = phi + penalty;
